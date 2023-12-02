6d7605a18a330878461e05d1f4605fd017bc1fbc
Michael Carey <mpcarey@csu.fullerton.edu>
2023-12-02 03:59:31 -0800

    minor bugfix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-foreman start -m enrollment=3,primary=1,secondary=1,tertiary=1,krakend=1,dynamodb=1
+foreman start -m enrollment=3,primary=1,secondary=1,tertiary=1,krakend=1,dynamodb=1,notification=1
 
 # This command runs the SMTP server in debugging mode
 python -m aiosmtpd -n -d 
