8399a42334aa40aa164f6b3e669ab23732e6e0e4
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2023-12-12 15:12:09 -0800

    added command to procfile

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
 foreman start -m enrollment=3,primary=1,secondary=1,tertiary=1,krakend=1,dynamodb=1,notification=1,email-consumer=3,webhook-consumer=3,aiosmtpd=1
-
-# This command runs the SMTP server in debugging mode
-python -m aiosmtpd -n -d 
